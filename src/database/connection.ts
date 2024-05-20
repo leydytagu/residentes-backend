9d65a3da1e5e566b9f58573bb7a1be746b2d2a25
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-05-20 00:52:34 -0500

    Se agrega database conection y middle

---
+++ src/database/connection.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export const dbConnection = async () => {
+  try {
+    const dbUrl = process.env.DB_CONNECTION;
+
+    if (!dbUrl) {
+      throw new Error(
+        "Error en la conexion de la base de datos, no existe la url"
+      );
+    }
+    await mongoose.connect(dbUrl);
+    console.log("DB Online");
+  } catch (error) {
+    console.log(error);
+    console.log("Error en la conexión de la base de datos");
+  }
+};
---
+++ src/middlewares/validate-jwt.ts
@@ -0,0 +1,58 @@
+import { NextFunction, Request, Response } from "express";
+
+const jwt = require("jsonwebtoken");
+
+export interface CustomRequest extends Request {
+  _id?: number;
+}
+
+export const validateJWT = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("x-token");
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No hay token en la petición",
+    });
+  }
+  try {
+    const { _id } = jwt.verify(token, process.env.JWTSECRET);
+    req._id = _id;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token Invalido",
+    });
+  }
+};
+
+export const validateJWTPass = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("x-token-pass");
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No hay token en la petición",
+    });
+  }
+
+  try {
+    const { _id } = jwt.verify(token, process.env.JWT_SECRET_PASS);
+    req._id = _id;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token invalido",
+    });
+  }
+};
